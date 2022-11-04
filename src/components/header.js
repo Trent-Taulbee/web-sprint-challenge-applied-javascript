3c846199a9c3752d0cad19b6c5fd10ab95e49014
Trent Taulbee <taulbee.trent@gmail.com>
2022-11-04 00:08:52 -0400

    11/4 stopping point

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,33 @@ const Header = (title, date, temp) => {
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
   // The html tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
+  
   //  <div class="header">
   //    <span class="date">{ date }</span>
   //    <h1>{ title }</h1>
   //    <span class="temp">{ temp }</span>
   //  </div>
-  //
+  const header = document.createElement("header");
+  const headerTitle = document.createElement("h1");
+  const headerDate = document.createElement("span");
+  const tempHeader = document.createElement("span");
+
+  header.appendChild(headerDate);
+  header.appendChild(headerTitle);
+  header.appendChild(tempHeader);
+
+  header.classList.add("header");
+  headerDate.classList.add("date");
+  headerTitle.classList.add("title");
+  tempHeader.classList.add("temp");
+
+  headerTitle.textcontent = title;
+  headerDate.textcontent = date;
+  tempHeader.textcontent = temp;
+
+  
+
+
 }
 
 const headerAppender = (selector) => {
